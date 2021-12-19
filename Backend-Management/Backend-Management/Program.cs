a5a51e00af152d5ec6055312a68bd20ae343ad2c
Andrei Dobre <andrei.dobr3@gmail.com>
2021-12-19 15:33:49 +0200

    Add Local Config

--- Backend-Management/Backend-Management/Program.cs
+++ Backend-Management/Backend-Management/Program.cs
@@ -9,13 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connectionString = "Host=gdscupt.tech;Port=6969;Database=users-Management;Username=guest;Password=cocacola";
+var connectionString = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
